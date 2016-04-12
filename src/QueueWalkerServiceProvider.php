7eee88c3b7a8cf5128f1ed71f292f95b6c7849b6
Alexander Kireev <salopot@gmail.com>
2016-04-12 13:14:41 +0300

    Register ServiceProvider for commands

--- src/QueueWalkerServiceProvider.php
+++ src/QueueWalkerServiceProvider.php
@@ -1,12 +1,30 @@
 <?php
 namespace Salopot\QueueWalker;
 
+use Salopot\QueueWalker\Console\WalkCommand;
+
 class QueueWalkerServiceProvider extends \Illuminate\Support\ServiceProvider
 {
 
     public function register()
     {
-        //
+        $this->app->bind('walker', 'Salopot\QueueWalker\Walker');
+        $this->app->singleton('command.queue-walker.walk', function($app) {
+            return new WalkCommand($app['walker']);
+        });
+        $this->commands(['command.queue-walker.walk']);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            'command.queue-walker.walk'
+        ];
     }
 
 }
